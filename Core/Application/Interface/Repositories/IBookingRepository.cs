813b70d6005460071609c4c3334023abab539610
hasbiyallah01 <oyebohm@gmail.com>
2025-06-15 13:30:31 +0100

    addition of booking repository to make reference to the booking for payment

---
+++ Core/Application/Interface/Repositories/IBookingRepository.cs
@@ -0,0 +1,10 @@
+﻿using Agricon.Core.Model.Entities;
+using Agricon.Infrastructure.Repository;
+
+namespace Agricon.Core.Application.Interface.Repositories
+{
+    public interface IBookingRepository
+    {
+        Task<Booking?> GetByIdAsync(string Id);
+    }
+}
---
+++ Infrastructure/Repository/BookingRespository.cs
@@ -0,0 +1,24 @@
+﻿using Agricon.Core.Application.Interface.Repositories;
+using Agricon.Core.Model.Entities;
+using Agricon.Infrastructure.AppContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Agricon.Infrastructure.Repository
+{
+    public class BookingRespository : IBookingRepository
+    {
+        private readonly AgriconContext _context;
+
+        public BookingRespository(AgriconContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Booking?> GetByIdAsync(string Id)
+        {
+            return await _context.Bookings.FirstOrDefaultAsync(p => p.Id == Id);
+        }
+
+    }
+}
